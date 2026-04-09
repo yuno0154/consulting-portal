4af1f49156fc0540428f735859499be7521af726
yuno0154@gmail.com <yuno0154@gmail.com>
2026-04-10 08:58:58 +0900

    공지사항 수정

--- notices.js
+++ notices.js
@@ -15,7 +15,7 @@ const NOTICES_DATA = [
 1. 수행평가 개수 - 개수를 1~12개까지 자유 조정
 2. 항목5 - 자유학기제 제외 옵션 추가
 3. 유형 선택시 미리 보기 제공 - 유형 진술만으로 검토 내용을 파악하기 어렵다는 요청에 따라 컨설팅 문장을 미리보기 형태로 제공함. 
-4. 항목 1, 2, 3, 4의 유형별 진술 - 수정, 추가, 삭제가 있음. <a href="https://drive.google.com/drive/folders/1UljogUTq1RETDI3BF3uIDl1A_u_CVomz?usp=sharing" target="_blank" rel="noopener">구글 드라이브에 수정 내용 안내 최종 문서</a>를 확인하세요.`,
+4. 항목 1, 2, 3, 4의 유형별 진술 - 수정, 추가, 삭제가 있음. 수정내용_안내_최종(4.8).docx  <a href="https://drive.google.com/drive/folders/1UljogUTq1RETDI3BF3uIDl1A_u_CVomz?usp=sharing" target="_blank" rel="noopener">구글 드라이브에 수정 내용 안내 최종 문서</a>를 확인하세요.`,
     date: "2026.04.09",
     label: "New",
     priority: "normal"
